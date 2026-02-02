8fbf50a6d6fee4dc74efc7331a67983032ae8257
denis-barbouteau <119499698+denis-barbouteau@users.noreply.github.com>
2026-02-02 14:44:14 +0100

    compatibility redmine 6.1.1

--- lib/redmine_workflow_hidden_fields/workflow_permission_patch.rb
+++ lib/redmine_workflow_hidden_fields/workflow_permission_patch.rb
@@ -5,7 +5,7 @@ module RedmineWorkflowHiddenFields
         # The filters are part of validators are raw, they can be skipped with the following way.
         rule_inclusion_validation = base._validators[:rule].find{ |validator| validator.is_a? ActiveModel::Validations::InclusionValidator }
         base._validators[:rule].delete(rule_inclusion_validation)
-        filter = base._validate_callbacks.find{ |c| c.raw_filter == rule_inclusion_validation }.filter
+        filter = base._validate_callbacks.find{ |c| c.filter.to_s == rule_inclusion_validation.to_s }.filter
         skip_callback :validate, filter            
 
         validates_inclusion_of :rule, :in => %w(readonly required hidden)
